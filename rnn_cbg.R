9daffd6bae4349fffd475381838bd0bc2a99141d
chris sainsbury <chris@sainsbury.im>
2017-05-18 22:54:37 +0100

    change definition of final time period

--- rnn_cbg.R
+++ rnn_cbg.R
@@ -32,8 +32,9 @@ DT<-data.table(DT)
 # cut_DT <- DT[admissionDurationDays >= 5 & admissionDurationDays < 30 & nCBGperAdmission > 20]
 
       ## need to be able to take all admissions greater than n days and look at hypo probability on day n
-      dayN_ofInterest = 5
-      dayN_ofInterestSeconds <- dayN_ofInterest * (60*60*24)
+      dayN_ofInterest = 4
+      daySeconds = (60*60*24)
+      dayN_ofInterestSeconds <- dayN_ofInterest * daySeconds
       
       # limit dataset to admissions that will have the required data ie at least 5 days of data
       cut_DT <- DT[admissionDurationDays > dayN_ofInterest]
@@ -70,40 +71,41 @@ DT<-data.table(DT)
     return(rep(flagPositiveResult, length(yyyy)))
   }
   
-  flag_CBGbelow_n_in24h_before_LastTime <- function(flagWithin_24h_prior_LastTime, yyyy, n) {
-    
-    lastimeValues <- yyyy[flagWithin_24h_prior_LastTime == 1]
-    returnVals <- ifelse(lastimeValues < n, 1, 0)
-    
-    flagPositiveResult <- ifelse(sum(returnVals) > 0, 1, 0)
-    
-    return(rep(flagPositiveResult, length(yyyy)))
-  }
+  # flag_CBGbelow_n_in24h_before_LastTime <- function(flagWithin_24h_prior_LastTime, yyyy, n) {
+  #   
+  #   lastimeValues <- yyyy[flagWithin_24h_prior_LastTime == 1]
+  #   returnVals <- ifelse(lastimeValues < n, 1, 0)
+  #   
+  #   flagPositiveResult <- ifelse(sum(returnVals) > 0, 1, 0)
+  #   
+  #   return(rep(flagPositiveResult, length(yyyy)))
+  # }
+  # 
+  # isDead_n_years <- function(cut_DT_ID, dateplustime1, n) {
+  #   
+  #   if (nrow(deathFrameDT[ID == cut_DT_ID]) > 0) {
+  #   isDead <- deathFrameDT[ID == cut_DT_ID]$isDead
+  #   deathDate <- deathFrameDT[ID == cut_DT_ID]$deathDateUnix
+  #   
+  #   returnValue <- ifelse(deathDate > 0 & deathDate <= (min(dateplustime1) + n * (60*60*24*365.25)), 1, 0)
+  #   
+  #   returnValue <- rep(returnValue, length(dateplustime1))
+  #   }
+  #   
+  #   if (nrow(deathFrameDT[ID == cut_DT_ID]) == 0) { returnValue <- rep(0, length(dateplustime1)) }
+  #   
+  #   return(returnValue)
+  #   
+  # }
   
-  isDead_n_years <- function(cut_DT_ID, dateplustime1, n) {
-    
-    if (nrow(deathFrameDT[ID == cut_DT_ID]) > 0) {
-    isDead <- deathFrameDT[ID == cut_DT_ID]$isDead
-    deathDate <- deathFrameDT[ID == cut_DT_ID]$deathDateUnix
-    
-    returnValue <- ifelse(deathDate > 0 & deathDate <= (min(dateplustime1) + n * (60*60*24*365.25)), 1, 0)
-    
-    returnValue <- rep(returnValue, length(dateplustime1))
-    }
-    
-    if (nrow(deathFrameDT[ID == cut_DT_ID]) == 0) { returnValue <- rep(0, length(dateplustime1)) }
-    
-    return(returnValue)
-    
-  }
+  cut_DT[, c("flagWithinLastTime") := (ifelse( dateplustime1 >= (min(dateplustime1) + ((dayN_ofInterest - 1) * daySeconds)) & dateplustime1 < (min(dateplustime1) + ((dayN_ofInterest) * daySeconds)), 1, 0)) , by=.(ID, admissionNumberFlag)]
   
-  cut_DT[, c("flagWithinLastTime") := (ifelse(dateplustime1 >= (min(dateplustime1) + ((max(dateplustime1) - min(dateplustime1)) - timePeriodSeconds)), 1, 0)) , by=.(ID, admissionNumberFlag)]
   cut_DT[, c("lessThan4_withinLastTime") := flag_CBGbelow_n_inLastTime(flagWithinLastTime, yyyy, 4), by=.(ID, admissionNumberFlag)]
   cut_DT[, c("lessThan3_withinLastTime") := flag_CBGbelow_n_inLastTime(flagWithinLastTime, yyyy, 3), by=.(ID, admissionNumberFlag)]
   cut_DT[, c("lessThan2p88_withinLastTime") := flag_CBGbelow_n_inLastTime(flagWithinLastTime, yyyy, 2.88), by=.(ID, admissionNumberFlag)]
   
-  cut_DT[, c("flagWithin_24h_prior_LastTime") := (ifelse((dateplustime1 >= (min(dateplustime1) + ((max(dateplustime1) - min(dateplustime1)) - 2*timePeriodSeconds))) & (dateplustime1 < (min(dateplustime1) + ((max(dateplustime1) - min(dateplustime1)) - timePeriodSeconds))), 1, 0)) , by=.(ID, admissionNumberFlag)]
-  cut_DT[, c("lessThan4_within_24h_prior_LastTime") := flag_CBGbelow_n_in24h_before_LastTime(flagWithinLastTime, yyyy, 4), by=.(ID, admissionNumberFlag)]
+  # cut_DT[, c("flagWithin_24h_prior_LastTime") := (ifelse((dateplustime1 >= (min(dateplustime1) + ((max(dateplustime1) - min(dateplustime1)) - 2*timePeriodSeconds))) & (dateplustime1 < (min(dateplustime1) + ((max(dateplustime1) - min(dateplustime1)) - timePeriodSeconds))), 1, 0)) , by=.(ID, admissionNumberFlag)]
+  # cut_DT[, c("lessThan4_within_24h_prior_LastTime") := flag_CBGbelow_n_in24h_before_LastTime(flagWithin_24h_prior_LastTime, yyyy, 4), by=.(ID, admissionNumberFlag)]
   
   
   # for death analysis run this
@@ -117,6 +119,15 @@ DT<-data.table(DT)
   colnames(report_y_hypo4) <- c("ID", "hypo_4")
   report_y_hypo3 <- data.frame(cut_DT[flagLastCBG == 1]$ID, cut_DT[flagLastCBG == 1]$lessThan3_withinLastTime)
   colnames(report_y_hypo3) <- c("ID", "hypo_3")
+  
+  # report_y_hypo4_24hPrior <- data.frame(cut_DT[flagLastCBG == 1]$ID, cut_DT[flagLastCBG == 1]$lessThan4_within_24h_prior_LastTime)
+  # colnames(report_y_hypo4_24hPrior) <- c("ID", "hypo_4")
+  
+  
+  ## code to test using prior hypo as a predictor
+  # test <- report_y_hypo4[39502:49346, ]; test_24hprior <- report_y_hypo4_24hPrior[39502:49346, ]
+  # library(pROC); auc(test$hypo_4, test_24hprior$hypo_4)
+
   ##****## report_y_death_3y <- data.frame(cut_DT[firstCBGperID == 1]$ID, cut_DT[firstCBGperID == 1]$isDead_3y)
   ##****## colnames(report_y_death_3y) <- c("ID", "dead_3y")
   
@@ -168,7 +179,7 @@ DT<-data.table(DT)
   # y = approx(x$scaled_dateplustime1, x$yyyy, n = 1000)
 
   # ensure that report_y in same order as process_X
-  id_diff <- process_X[, 1] - report_y_death_3y[, 1]
+  id_diff <- process_X[, 1] - report_y_hypo4[, 1]
   ifelse(sum(id_diff) > 0, sum(id_diff), print("id match"))
   
   # save out input data (X) and (y)
@@ -179,19 +190,19 @@ DT<-data.table(DT)
   
   save_y_hypo4 <- report_y_hypo4$hypo_4
   save_y_hypo3 <- report_y_hypo3$hypo_3
-  save_y_dead3 <- report_y_death_3y$isDead_3y
+  # save_y_dead3 <- report_y_death_3y$isDead_3y
   
   
   # randomisingSequence <- runif(nrow(report_y_hypo4), 0, 1)
   
   ## writeout files for tensorflow
   # write out sequence for analysis
-  write.table(save_X, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/5thday_processX.csv", sep=",", row.names = FALSE)
+  write.table(save_X, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/4thday_processX.csv", sep=",", row.names = FALSE)
   
   
   # write out dep variable (y)
-  write.table(save_y_hypo4, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/5thday_report_y_hypo4.csv", sep = ",", row.names = FALSE)
-  write.table(save_y_hypo3, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/5thday_report_y_hypo3.csv", sep = ",", row.names = FALSE)
+  write.table(save_y_hypo4, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/4thday_report_y_hypo4.csv", sep = ",", row.names = FALSE)
+  write.table(save_y_hypo3, file = "~/R/_workingDirectory/rnn_inpatient_cbg/data/4thday_report_y_hypo3.csv", sep = ",", row.names = FALSE)
   
   
   
